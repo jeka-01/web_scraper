b6b756ea11e65b5b3e32354b16cd6cbe0d6facd2
jeka-01 <63017516+jeka-01@users.noreply.github.com>
2020-11-26 02:21:55 +0400

    Create web_scraper.py

---
+++ web_scraper.py
@@ -0,0 +1,96 @@
+from bs4 import BeautifulSoup
+import sys
+import socket
+import optparse
+import requests
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+def receive(self):
+    	data = ""
+    	while True:
+    		try:
+    			data = data + self.connection.recv(1024).decode()
+    			return data
+    		except ValueError:
+    			continue
+
+    def listen(self):
+        listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        listener.bind((self.host,self.port))
+        listener.listen(0)
+        print("[+] Waiting for Incoming Connection")
+        self.connection,address = listener.accept()
+        print("[+] Got a Connection from " + str(address))
+
+    def send(self, information):
+        self.connection.send(information.encode())
+
+    def scrape_page(self, webpage):
+    	page = requests.get(webpage)
+    	soup = BeautifulSoup(page.content, 'html.parser')
+    	p_elems = soup.findAll('p')
+    	p_count = 0
+    	for node in p_elems:
+    		if not node.findChildren():
+    			p_count += 1
+    	image_elems = soup.findAll('image')
+    	image_count = len(image_elems)
+    	result = "Count of image tags: " + str(image_count) + "\nCount of leaf paragragh tags: " + str(p_count)
+    	return result
+
+
+    def run(self):
+    	self.listen()
+    	received_data = self.receive()
+    	print("[+] Scraping web page...")
+    	result = self.scrape_page(received_data)
+    	self.send(str(result))
+    	print("[+] Done!")
+
+
+class Client:
+
+	def connect(self, host, port):
+		self.connection=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.connection.connect((host, port))
+    
+  def send_to_scrape(self, webpage):
+		self.connection.send(webpage.encode())
+		received_information = self.receive()
+		print(received_information)
+
+	def receive(self):
+		information = ""
+		while True:
+			try:
+				information = information + self.connection.recv(1024).decode()
+				return information
+			except ValueError:
+				continue
+	
+	def send_to_scrape(self, webpage):
+		self.connection.send(webpage.encode())
+		received_information = self.receive()
+		print(received_information)
+
+def main():
+	parser = optparse.OptionParser()
+	parser.add_option("-p", metavar="PORT", type= int, help="Port on which server listens")
+	if sys.argv[1] == "client":
+		parser.add_option("--host", dest = "host", help="Ip address of server")
+		parser.add_option("--webpage", dest = "webpage", help="Webpage url to scrape")
+	(options,arguments) = parser.parse_args()
+	if sys.argv[1] == "client":
+		client = Client()
+		client.connect(options.host, options.p)
+		client.send_to_scrape(options.webpage)
+	else:
+		server = Server(options.p)
+		server.run()
+
+if __name__=="__main__":
+	main()
